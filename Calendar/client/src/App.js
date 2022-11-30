1f37a719c6577bb597a3d557a1b663b56dcb4a68
hahahaday12 <hahahaday12@naver.com>
2022-11-30 23:55:16 +0900

    Life-Calendar Private Page 추가

--- Calendar/client/src/App.js
+++ Calendar/client/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import { BrowserRouter , Route , Routes} from 'react-router-dom'
+import PrivateRoute from "./privateRoute";
 //import LoinButton from './1Page/loginButton/index.js';
 import Onepage from './1Page';
 import TwoPage from './2Page';
+import ThirdPage from './3Page';
 
 function App() {
   return (
     <>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Onepage/>} />
-      <Route path="/twopage" element={<TwoPage/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={
+          <PrivateRoute component={<ThirdPage/>} status={<Onepage/>}/>
+        }/>
+        <Route path="/twopage" element={<TwoPage/>} />
+        <Route path="/thirdpage" element={
+        <PrivateRoute component={<ThirdPage/>}/>
+        }/>
+      </Routes>
     </BrowserRouter>
     </>
   );
 }
-
 export default App;
