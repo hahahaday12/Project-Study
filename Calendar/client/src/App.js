dcc3843347158a15b7a8d64f54a6cd3a804fa57f
hahahaday12 <hahahaday12@naver.com>
2022-12-11 23:13:21 +0900

    Life-Calendar Private 4Page 추가

--- Calendar/client/src/App.js
+++ Calendar/client/src/App.js
@@ -1,26 +1,37 @@
 import './App.css';
-import { BrowserRouter , Route , Routes} from 'react-router-dom'
+import {  Route , Routes} from 'react-router-dom'
+import {RecoilRoot} from 'recoil';
 import PrivateRoute from "./privateRoute";
 //import LoinButton from './1Page/loginButton/index.js';
 import Onepage from './1Page';
 import TwoPage from './2Page';
 import ThirdPage from './3Page';
+import Fourpage from './4Page';
+
+
 
 function App() {
   return (
+    <div className="App">
     <>
-    <BrowserRouter>
+    <RecoilRoot>
       <Routes>
         <Route path="/" element={
           <PrivateRoute component={<ThirdPage/>} status={<Onepage/>}/>
         }/>
         <Route path="/twopage" element={<TwoPage/>} />
+
         <Route path="/thirdpage" element={
-        <PrivateRoute component={<ThirdPage/>}/>
+            <PrivateRoute component={<ThirdPage/>}/>
         }/>
+        
+        <Route path="/fourpage" element={
+            <PrivateRoute component={<Fourpage/>}/>
+          }/>
       </Routes>
-    </BrowserRouter>
+    </RecoilRoot>
     </>
+    </div>
   );
 }
 export default App;
