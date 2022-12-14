213b25d43abc568a05f74ca03120c9d4105e6304
hahahaday12 <hahahaday12@naver.com>
2022-12-14 23:59:57 +0900

    선언되지 않은  import  삭제

--- Calendar/client/src/index.js
+++ Calendar/client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { Provider } from 'react-redux';
-// import store, { sagaRun } from "./store/_index";
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -11,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
 
-
+<BrowserRouter>
+        <RecoilRoot>
     <App />
-
+</RecoilRoot>
+</BrowserRouter>
 
 );
 
