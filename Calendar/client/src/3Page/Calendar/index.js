397d01928d2b1ce49dca0311ed156aee815f8ab4
hahahaday12 <hahahaday12@naver.com>
2023-01-02 23:41:47 +0900

    Calendar 에 diaryColor 값 표시 하도록 수정중

--- Calendar/client/src/3Page/Calendar/index.js
+++ Calendar/client/src/3Page/Calendar/index.js
@@ -1,31 +1,63 @@
-import React from "react";
+import { useState, React, useEffect }from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import googleCalendarPlugin from '@fullcalendar/google-calendar'
+import {API_URL} from '../../Common/Common'
+import axios from 'axios';
 import "./index.css";
 
 
-// export default function App() {
+const FCalendar = () => {
 
-const Full = () => {
+  const [ViewData, setViewData] = useState({
+     id:""
+    ,title:""
+    ,content:""
+    ,date: new Date()
+    ,color:""
+  });
+
+  const search = (params) => { 
+    let date = null;
+    if(params){
+      date = params;
+    }else{
+      date = new Date();
+    };
+    axios.get(API_URL+ '/diary?year='+String(date.getFullYear())+'&month='+String(date.getMonth()+1))
+    .then((response) => {
+      setViewData(response.data.data);
+    });
+  };
+
+  useEffect(() => {
+    search();
+    console.log(ViewData)
+  },[]);
+
+  const EventDetail = (event, el) => {
+    console.log(event);
+    console.log(el);
+  };
+
   const apiKey = process.env.REACT_APP_API_KEY;
   //console.log(apiKey)
     return (
       <div className="CalendarBox">
         <div className='container'>
-
         <div className='App'>
         <FullCalendar
           plugins={[dayGridPlugin, googleCalendarPlugin]}
           googleCalendarApiKey={apiKey}
           initialView="dayGridMonth"
-          events=
-            {{googleCalendarId: 'ko.south_korea#holiday@group.v.calendar.google.com'}}
+          eventLimit="false"
+          eventSources = {{googleCalendarId:'ko.south_korea#holiday@group.v.calendar.google.com', className:'ko_event'}}
+          events={ViewData}
+          eventDidMount={EventDetail}
           />
         </div>
         </div>
       </div>
-            );
-    };
-  
-  export default Full;
+    );
+};
+export default FCalendar;
